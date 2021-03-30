bbfc91687fe9fd6b102cadc88969956832757848
Godoy Godoy <alejandogodoy22@gmail.com>
2021-03-29 21:28:22 -0300

    Variables and Operators

---
+++ excercise1.js
@@ -0,0 +1,23 @@
+// Exercise N° 1/ A
+
+var num = 2;
+var num1 = 2;
+var result = num + num1;
+
+console.log("The result is ", result);
+
+//Exercise N° 1/ B
+
+var firstWord = "Hola ";
+var secondWord = "mundo.";
+var sentence = firstWord + secondWord;
+
+console.log("The concatenated word is :", sentence);
+
+//Exercise 1/ C
+
+var word1 = "Alejandro";
+var word2 = "Godoy";
+var letters = word1.length + word2.length;
+
+console.log("The length of letters is: ", letters);
